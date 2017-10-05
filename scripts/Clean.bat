aa3a188ec85e09056eec870fbff4a0dac7d9e9c2
Simon Coenen Gobo <sim2.coenen@gmail.com>
2017-10-05 16:25:42 +0100

    Changed premake commands to work from outside the 'scripts' directory

--- scripts/Clean.bat
+++ scripts/Clean.bat
@@ -1,6 +1,4 @@
 echo off
 cls
-
-echo Building for Visual Studio 2017...
 premake5 clean
 pause
--- scripts/make.bat
+++
@@ -1,8 +0,0 @@
-echo off
-cls
-
-echo Building for Visual Studio 2017...
-premake5 vs2017
-echo Building for Visual Studio 2015...
-premake5 vs2015
-pause
