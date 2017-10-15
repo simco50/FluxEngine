a44fb796f412a7b1abe5f09bb4681d891c607356
Simon Coenen <sim2.coenen@gmail.com>
2017-10-15 12:55:14 +0100

    Test 4

--- FluxPak/CreatePakFile.bat
+++ FluxPak/CreatePakFile.bat
@@ -1,2 +1,2 @@
 SET checkoutDir=%~1
-"%~dp0/FluxPak/PakFileCompression.exe" -r "FluxPak/ResponseFile.txt" -p "%~dp0/FluxGame/Resources.pak" -b "%checkoutDir%FluxGame/Resources/"
+"FluxPak/PakFileCompression.exe" -r "FluxPak/ResponseFile.txt" -p "FluxGame/Resources.pak" -b "%checkoutDir%FluxGame/Resources/"
